566e9315cf89f7125b8c23d51a33352690eaae3d
abdulsamad <abdulsamad.cs15@gmail.com>
2021-12-28 23:51:59 +0500

    Embedded chat engine code.

--- src/App.js
+++ src/App.js
@@ -1,2 +1,16 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height='100vh'
+            userName='abdulsamad'
+            userSecret='12345678'
+            projectID='bb8db61a-b64b-428f-886b-3fdc21cbbef5'
+        />
+    )
+
+}
+
+export default App;  

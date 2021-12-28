7f65824bf8e47e2a4673eb862929dda26eed2911
abdulsamad <abdulsamad.cs15@gmail.com>
2021-12-29 00:11:48 +0500

    ChatFeed component created.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
+import ChatFeed from './components/ChatFeed';
+
 const App = () => {
     return (
         <ChatEngine 
@@ -8,6 +10,7 @@ const App = () => {
             userName='abdulsamad'
             userSecret='12345678'
             projectID='bb8db61a-b64b-428f-886b-3fdc21cbbef5'
+            renderChatFeed = { (chatAppProps ) => <ChatFeed {... chatAppProps}/>}
         />
     )
 
